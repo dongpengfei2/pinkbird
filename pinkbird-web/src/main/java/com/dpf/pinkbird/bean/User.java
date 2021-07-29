16d5962139324cc29085d59ee04092ec282efd44
dongpengfei1 <dongpengfei1@shihuituan.com>
2021-07-29 11:36:19 +0800

    mybatis配置

--- pinkbird-web/src/main/java/com/dpf/pinkbird/bean/User.java
+++ pinkbird-web/src/main/java/com/dpf/pinkbird/bean/User.java
@@ -3,12 +3,28 @@ package com.dpf.pinkbird.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    Integer id;
-    String username;
-    String password;
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Boolean stauts;
+
+    private Boolean isDeleted;
+
+    private Date createTime;
+
+    private Date editTime;
+
+    private String creator;
+
+    private String editor;
+
 }
