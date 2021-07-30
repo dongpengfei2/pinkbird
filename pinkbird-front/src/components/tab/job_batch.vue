606da4bec9e8d783c28548c127ce6dfadbef0592
dongpengfei1 <dongpengfei1@shihuituan.com>
2021-07-30 11:30:15 +0800

    添加搜索选项

--- pinkbird-front/src/components/tab/job_batch.vue
+++ pinkbird-front/src/components/tab/job_batch.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <h3>批作业</h3>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>作业列表</el-breadcrumb-item>
+      <el-breadcrumb-item>JAR任务</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片试图区域 -->
+    <el-card>
+      <!-- 搜索与添加区域 -->
+      <el-row gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">新增任务</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
