f2927e531602efce7b5156fb439852ec3e0f1405
dongpengfei1 <dongpengfei1@shihuituan.com>
2021-08-02 11:19:43 +0800

    分页查询

--- pinkbird-web/src/main/java/com/dpf/pinkbird/mapper/BatchJobMapper.java
+++ pinkbird-web/src/main/java/com/dpf/pinkbird/mapper/BatchJobMapper.java
@@ -3,13 +3,16 @@ package com.dpf.pinkbird.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.dpf.pinkbird.bean.BatchJob;
+import com.dpf.pinkbird.bean.Pagination;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
+@Mapper
 public interface BatchJobMapper extends BaseMapper<BatchJob> {
-    @Select("select * from job_batch")
-    IPage<BatchJob> selectPage(IPage<BatchJob> page);
+    @Select("select * from job_batch where lower(job_id) like concat('%', lower(#{page.query}),'%') or lower(job_name) like concat('%', lower(#{page.query}),'%')")
+    IPage<BatchJob> selectPage(@Param("page") Pagination<BatchJob> page);
     @Update("update job_batch set job_status=#{jobStatus} where job_id=#{jobId}")
     void updateJobStatus(@Param("jobId") String jobId, @Param("jobStatus") Integer jobStatus);
 }
